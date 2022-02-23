825ca9e9e274cd426e2788593823b24964c47b21
almohdali <almohdali@gmail.com>
2022-02-23 21:19:56 +0300

    add create profile save

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from re import T
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -12,3 +14,10 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user)
+    
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+       if created:
+           Profile.objects.create(
+               user = instance
+           )
