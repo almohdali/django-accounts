ec5a9951d4054674132d0de92576d14faf5cb97f
almohdali <almohdali@gmail.com>
2022-02-24 05:39:58 +0300

    add profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Profile
+from .forms import  SignupForm
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.changed_data['username']
+            password = form.changed_data['password2']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('/accounts/profile')
+    else:
+        form = SignupForm()
+        return render(request,'registration/signup.html',
+                      {
+                        'form':form,
+                      })
+
+
+
+def profile(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+        return render(request,'registration/signup.html',{})
